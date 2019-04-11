14fec6a1a0cbc5d5659d4c35c93bb51927fe6c7f
Dan Fettke <daniel.fettke@outlook.com>
2019-04-10 20:48:29 -0700

    Adding support for eatingwell.com

--- WebsiteScraper.py
+++ WebsiteScraper.py
@@ -187,6 +187,40 @@ class BettyCrockerScrapper():
         return recipeData
 
 
+class EatingWellScrapper():
+
+    def ExtractIngredients(self, soup):
+        ingredients = []
+        for ingredient in soup.find_all('span', itemprop='ingredients'):
+            ingredients.append(ingredient.string.strip())
+        return ingredients
+
+
+    def ExtractInstructions(self, soup):
+
+        instructions = ''
+        for instuction in soup.find_all('span', class_="recipeDirectionsListItem"):
+            # Eating well has an extra recipe direction that has no text and doesn't
+            # pertain to the instructions.
+            # If this attribute is found ignore it.
+            if 'ng-bind-html' not in instuction.attrs:
+                instructions += '{}\n'.format(instuction.string.strip())
+        return instructions
+
+
+    def ExtractRecipeName(self, soup):
+
+        return soup.find('meta', property='og:title')['content']
+
+
+    def ExtractRecipeData(self, soup):
+        recipeData = {}
+        recipeData['name'] = self.ExtractRecipeName(soup)
+        recipeData['recipeIngredient'] = self.ExtractIngredients(soup)
+        recipeData['recipeInstructions'] = self.ExtractInstructions(soup)
+        return recipeData
+
+
 class RecipeObjectClass:
 
     def __init__(self, recipeObj=None):
@@ -215,6 +249,8 @@ class RecipeObjectClass:
             return TheKitchnScrapper
         elif 'bettycrocker' in url:
             return BettyCrockerScrapper
+        elif 'eatingwell' in url:
+            return EatingWellScrapper
         else:
             return JsonScrapper
 
---
+++ tests/test_EatingWellScrapper.py
@@ -0,0 +1,51 @@
+import unittest
+import bs4 as bs
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from WebsiteScraper import EatingWellScrapper
+
+class KitchenScrapperTestClass(unittest.TestCase):
+
+
+    @classmethod
+    def setUpClass(self):
+         with open(os.path.join(os.path.dirname(__file__),'testHtml/eatingWellTest1.html'),'r') as inHtml:
+            soup = bs.BeautifulSoup(inHtml.read(), 'lxml')
+            self.recipeData = EatingWellScrapper().ExtractRecipeData(soup)
+
+    def test_RecipeName(self):
+
+        actualName = 'Gluten-Free Pumpkin Waffles Recipe'
+        testName = self.recipeData['name']
+        self.assertEqual(actualName, testName, 'Recipe name mismath.\nExpected {}\nGot {}'.format(actualName, testName))
+
+
+    def test_RecipeIngredients(self):
+
+        actualIngredients = ['Nonstick cooking spray',
+                             '½ cup gluten-free all-purpose flour',
+                             '½ cup vanilla-flavor whey protein powder',
+                             '¼ cup coconut flour', '¼ cup flaxseed meal',
+                             '¼ cup sugar (see Tips)',
+                             '2 teaspoons arrowroot or cornstarch',
+                             '2 teaspoons baking powder',
+                             '2 teaspoons ground cinnamon',
+                             '½ teaspoon baking soda',
+                             '½ teaspoon ground ginger',
+                             '1 cup unsweetened almond milk',
+                             '¾ cup canned pumpkin puree (see Tips)',
+                             '3 egg whites', '1 tablespoon grapeseed oil',
+                             '1 teaspoon vanilla', 'Sugar-free maple syrup (optional)',
+                             'Frozen light whipped dessert topping, thawed (optional)',
+                             'Ground cinnamon (optional)']
+        testIngredients = self.recipeData['recipeIngredient']
+        self.assertTrue(isinstance(testIngredients, list), 'Ingredients should be a list')
+        self.assertEqual(actualIngredients, testIngredients, 'Recipe Ingredient Mismatch')
+
+
+    def test_RecipeInstructions(self):
+
+        testInstructions = self.recipeData['recipeInstructions']
+        self.assertTrue(isinstance(testInstructions, str), 'Recipe Instructions should be a string')
