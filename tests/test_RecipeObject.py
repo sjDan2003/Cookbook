5df3477f0da2b46f91f88b2f397003c27c0cc336
Dan Fettke <daniel.fettke@outlook.com>
2019-06-06 21:59:59 -0700

    Adding examples of how to mock urllib in a context manager

--- tests/test_RecipeObject.py
+++ tests/test_RecipeObject.py
@@ -1,10 +1,41 @@
 import unittest
+from unittest.mock import patch, Mock
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from WebsiteScraper import RecipeObjectClass
+from urllib.error import HTTPError
 
 class RecipeObjectTestClass(unittest.TestCase):
 
-    pass
+    @patch('WebsiteScraper.urllib.request.Request')
+    @patch('WebsiteScraper.urllib.request.urlopen')
+    def test_GetHtmlData_Status_200(self, mock_urlopen, mock_request):
+
+        testString = 'This is a test string'
+        testUrl = 'www.mockurl.com'
+
+        # Simply stub out the request so it doesn't try and make a request with the URL
+        mock_request = Mock()
+
+        mock_urlopen.return_value.__enter__.return_value.read.side_effect = [testString]
+        recipeObject = RecipeObjectClass()
+        source = recipeObject.GetHtmlData(testUrl)
+        print(source)
+
+    @patch('WebsiteScraper.urllib.request.Request')
+    @patch('WebsiteScraper.urllib.request.urlopen')
+    def test_GetHtmlData_Status_400(self, mock_urlopen, mock_request):
+
+        testString = ''
+        testUrl = 'www.mockurl.com'
+        testCode = 400
+
+        # Simply stub out the request so it doesn't try and make a request with the URL
+        mock_request = Mock()
+
+        mock_urlopen.return_value.__enter__.return_value.read.side_effect = HTTPError(testUrl, testCode,None, None, None)
+        recipeObject = RecipeObjectClass()
+        source = recipeObject.GetHtmlData(testUrl)
+        self.assertEqual(testString, source, 'GetHtmlData should return blank string for errors')
